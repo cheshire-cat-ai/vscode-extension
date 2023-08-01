c93778a5eba74bfc7f487716930b3d9e00511d26
Dany <zdanymc@gmail.com>
2023-08-01 10:29:14 +0200

    removed llm settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,63 +1,16 @@
 import { window, ExtensionContext, workspace, commands, Range } from 'vscode';
 import { CatClient } from 'ccat-api';
 
-const ModelConfig = [
-	"ChatGPT | gpt-3.5-turbo",
-	"GPT-3 | text-davinci-003",
-	"Cohere | command",
-	"HuggingFace Hub | starcoder"
-] as const;
-
 const AcceptedConfig = [
 	"LLMOpenAIChatConfig",
 	"LLMOpenAIConfig",
 	"LLMCohereConfig",
-	"LLMHuggingFaceHubConfig"
+	"LLMHuggingFaceHubConfig",
+	"LLMAzureOpenAIConfig",
+	"LLMAzureOpenAIChatConfig"
 ] as const;
 
 const extId = "CheshireCatAI.cheshire-cat-ai";
-
-const getModelConfig = (llm: string, apiKey: string) => {
-	const modelsConfig = {
-		"ChatGPT | gpt-3.5-turbo": {
-			name: "LLMOpenAIChatConfig",
-			requestBody: {
-				"openai_api_key": apiKey,
-				"model_name": "gpt-3.5-turbo"
-			}
-		},
-		"GPT-3 | text-davinci-003": {
-			name: "LLMOpenAIConfig",
-			requestBody: {
-				"openai_api_key": apiKey,
-				"model_name": "text-davinci-003"
-			}
-		},
-		"Cohere | command": {
-			name: "LLMCohereConfig",
-			requestBody: {
-				"cohere_api_key": apiKey,
-				"model": "command"
-			}
-		},
-		"HuggingFace Hub | starcoder": {
-			name: "LLMHuggingFaceHubConfig",
-			requestBody: {
-				"repo_id": "bigcode/starcoder",
-				"huggingfacehub_api_token": apiKey
-			}
-		}
-	}
-	const model = llm as keyof typeof modelsConfig
-	if (!ModelConfig.includes(model)) {
-		return {
-			name: "LLMDefaultConfig",
-			requestBody: {}
-		}
-	} else {
-		return modelsConfig[model]
-	}
-}
 
 const getConfig = () => workspace.getConfiguration('CheshireCatAI')
 
@@ -71,6 +24,7 @@ export async function activate(context: ExtensionContext) {
 		authKey: ccatConfig.AuthKey,
 		baseUrl: ccatConfig.BaseUrl,
 		port: ccatConfig.Port,
+		secure: ccatConfig.Secure,
 		ws: {
 			path: ccatConfig.WebsocketPath,
 			onFailed: (err) => {
@@ -91,30 +45,20 @@ export async function activate(context: ExtensionContext) {
 	
 	if (!selected || !AcceptedConfig.includes(selected.name as typeof AcceptedConfig[number])) {
 		isCompatible = false
-		window.showWarningMessage("Your LLM configuration is not supported! Please, update your settings");
-		commands.executeCommand('workbench.action.openSettings', `@ext:${extId}`);
-	} else if (!hasPlugin) {
+		window.showWarningMessage("Your LLM configuration is not supported! Please, update your cat's settings");
+	}
+
+	if (!hasPlugin) {
 		commands.executeCommand('workbench.action.openWalkthrough', `${extId}#firstInstall`, true);
-	} else {
-		const updatedConfig = getConfig()
-		console.log(selected)
-		//updatedConfig.update('CheshireCatAI.LanguageModel', selected.value)
 	}
 
 	context.subscriptions.push(commands.registerCommand("cheshire-cat-ai.toSettings", () => {
 		commands.executeCommand('workbench.action.openSettings', `@ext:${extId}`);
 	}));
 
-	context.subscriptions.push(commands.registerCommand("cheshire-cat-ai.refreshLLM", async () => {
-		window.showWarningMessage("Updating LLM configuration...");
-		const updatedConfig = getConfig();
+	context.subscriptions.push(commands.registerCommand("cheshire-cat-ai.refreshConnection", () => {
 		cat.reset()
 		cat.init()
-		if (!isCompatible) {
-			let llmSetting = getModelConfig(updatedConfig.LanguageModel, updatedConfig.ApiKey);
-			await cat.api?.settingsLargeLanguageModel.upsertLlmSetting(llmSetting.name, llmSetting.requestBody);
-		}
-		window.showInformationMessage("LLM configuration updated successfully!");
 	}));
 
 	context.subscriptions.push(commands.registerCommand("cheshire-cat-ai.fetchPlugins", async () => {
@@ -130,6 +74,7 @@ export async function activate(context: ExtensionContext) {
 	context.subscriptions.push(commands.registerCommand("cheshire-cat-ai.commentCode", () => {
 		if (!hasPlugin) {
 			window.showErrorMessage("You didn't install the Code Commenter plugin!");
+			commands.executeCommand('workbench.action.openWalkthrough', `${extId}#firstInstall`, true);
 			return
 		}
 
@@ -147,36 +92,33 @@ export async function activate(context: ExtensionContext) {
 			// Get text selection
 			const selectionRange = new Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
 			const highlighted = editor.document.getText(selectionRange);
-			if (ModelConfig.includes(updatedConfig.LanguageModel, 3)) {
-				window.showErrorMessage("Automated commenting is only available with OpenAI models");
-			} else {
-				cat.send(highlighted, {
-					use_declarative_memory: false,
-					use_procedural_memory: false,
-					use_episodic_memory: false,
-					task: "comment"
-				});
-				
-				cat.onMessage(data => {
-					const json = JSON.parse(data.content);
-					window.showInformationMessage(`Detected language: ${json.language}`);
-					if (json.code) {
-						editor.edit(editBuilder => {
-							editBuilder.replace(selectionRange, json.code);
-						});
-					} else {
-						window.showErrorMessage("The highlighted text may not be valid code. Try again please");
-					}
-				}).onError(() => {
-					window.showErrorMessage("Something went wrong. Try again please");
-				});
-			}
+			cat.send(highlighted, {
+				use_declarative_memory: false,
+				use_procedural_memory: false,
+				use_episodic_memory: false,
+				task: "comment"
+			});
+			
+			cat.onMessage(data => {
+				const json = JSON.parse(data.content);
+				window.showInformationMessage(`Detected language: ${json.language}`);
+				if (json.code) {
+					editor.edit(editBuilder => {
+						editBuilder.replace(selectionRange, json.code);
+					});
+				} else {
+					window.showErrorMessage("The highlighted text may not be valid code. Try again please");
+				}
+			}).onError(() => {
+				window.showErrorMessage("Something went wrong. Try again please");
+			});
 		}
 	}));
 
 	context.subscriptions.push(commands.registerCommand("cheshire-cat-ai.makeFunction", () => {
 		if (!hasPlugin) {
 			window.showErrorMessage("You didn't install the Code Commenter plugin!");
+			commands.executeCommand('workbench.action.openWalkthrough', `${extId}#firstInstall`, true);
 			return
 		}
 
@@ -200,21 +142,12 @@ export async function activate(context: ExtensionContext) {
 				use_procedural_memory: false,
 				use_episodic_memory: false,
 				task: "function"
-			} as any);
+			});
 			
 			cat.onMessage(data => {
-				const updatedConfig = getConfig();
-				if (ModelConfig.includes(updatedConfig.LanguageModel, 2)) {
-					editor.edit(editBuilder => {
-						const createdFunction = `${highlighted}\n${data.content}`
-						editBuilder.replace(selectionRange, createdFunction);
-					});
-				}
-				else {
-					editor.edit(editBuilder => {
-						editBuilder.replace(selectionRange, data.content);
-					});
-				}
+				editor.edit(editBuilder => {
+					editBuilder.replace(selectionRange, data.content);
+				});
 			}).onError(() => {
 				window.showErrorMessage("Something went wrong. Try again please");
 			});
